22fb921dfc06936404e6f4ceb710951c5b014c7a
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2024-09-15 21:54:12 +0700

    Add files via upload

---
+++ tuan2/Ex1.java
@@ -0,0 +1,12 @@
+package tuan2;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Tên của bạn là: ");
+		String Ten = sc.nextLine();
+		System.out.println("hello "+ Ten);
+	}
+}
---
+++ tuan2/Ex3.java
@@ -0,0 +1,29 @@
+package tuan2;
+
+import java.util.Scanner;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập số thứ nhất : ");
+		int a = sc.nextInt();
+		System.out.println("Nhập số thứ hai : ");
+		int b = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Nhập toán tử : ");
+		char tt = sc.next().charAt(0);
+		int kq = 0;
+		switch (tt) {
+		case '+': kq = a + b; break;
+		case '-': kq = a - b; break;
+		case '*': kq = a * b; break;
+		case '/': kq = a / b; break;
+		case 'x': kq = a * b; break;
+		case ':': kq = a / b; break;
+		default:
+			System.out.println("Bạn đã nhập sai!!!");
+			System.exit(0);
+		}
+		System.out.println(a+""+ tt +"" + b + '=' + kq);
+	}
+}
