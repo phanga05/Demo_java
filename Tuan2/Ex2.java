93825a1804523b0caa6218a53cc6f3c5e6c07f04
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2024-09-29 20:43:58 +0700

    Mạnh Huy

---
+++ Tuan2/Ex2.java
@@ -0,0 +1,23 @@
+package Tuan2;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Họ Và Tên :");
+		String HoVaTen = sc.nextLine();
+		System.out.println("Tuổi: ");
+		int Tuoi = sc.nextInt();
+		System.out.println("Chiều Cao: ");
+		int ChieuCao = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Bạn có thích lập trình không? ");
+		String Ans = sc.nextLine();
+		System.out.println("Họ Và Tên :" + HoVaTen);
+		System.out.println("Tuổi: " + Tuoi);
+		System.out.println("Chiều Cao: " + ChieuCao);
+		System.out.println("Bạn có thích lập trình không? : " +Ans );
+	}
+}
