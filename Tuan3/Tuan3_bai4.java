a1192b3466aecea3cc407b42903611f2b60c144f
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2024-09-29 20:38:03 +0700

    Mạnh Huy

---
+++ Tuan3/Tuan3_bai4.java
@@ -0,0 +1,15 @@
+package Tuan3;
+
+import java.util.Scanner;
+
+public class Tuan3_bai4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập vào 3 số");
+		float a = sc.nextFloat();
+		float b = sc.nextFloat();
+		float c = sc.nextFloat();
+		float min = a < b ? (a < c ? a : c) : (b < c ? b : c);
+		System.out.println("Số nhỏ nhất trong 3 số là: "+ min);
+	}
+}
