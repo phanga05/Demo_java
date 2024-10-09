47a4e3ccb44f94b44ad58f58e0d4abb6d10614e9
ManhHuy <trandinhmanhhuy05@gmail.com>
2024-10-09 22:44:50 +0700

    s

---
+++ Tuan6/HinhChuNhat.java
@@ -0,0 +1,37 @@
+package Tuan6;
+
+import java.util.Scanner;
+
+public class HinhChuNhat {
+	protected float Dai;
+	protected float Rong;
+	
+	public HinhChuNhat(float dai, float rong) {
+		Dai = dai;
+		Rong = rong;
+	}
+	
+	public HinhChuNhat(HinhChuNhat hcn) {
+		this.Dai = hcn.Dai;
+		this.Rong = hcn.Rong;
+	}
+	
+	public HinhChuNhat() {
+	}
+	
+	public void Nhap() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập chiều dài :");
+		this.Dai = sc.nextFloat();
+		System.out.print("Nhập chiều rộng :");
+		this.Rong = sc.nextFloat();
+	}
+	
+	public void tinhDienTich() {
+		System.out.println("Diện tích hình chữ nhật là: "+ this.Dai*this.Rong);
+	}
+	
+	public void tinhChuVi() {
+		System.out.println("Chu vi hình chữ nhật là: "+ (this.Dai+this.Rong)*2);
+	}
+}
---
+++ Tuan6/package-info.java
@@ -0,0 +1,1 @@
+package Tuan6;
